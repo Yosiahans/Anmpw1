288f3b4930a05f7db3b176007e1f88237abd69b0
Yosiahans <s160421126@student.ubaya.ac.id>
2024-02-22 14:28:17 +0700

    SignUp act updated

--- app/src/main/java/com/example/anmp1/SignupActivity.kt
+++ app/src/main/java/com/example/anmp1/SignupActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // p
+        // q
+        //r
+        //s
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
     }
